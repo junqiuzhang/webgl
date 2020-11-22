9be3a36d2b1a7db9ac97b238567858f03608b079
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-22 16:41:36 +0800

    demo 1 canvas

--- src/demo-1/index.ts
+++ src/demo-1/index.ts
@@ -1,5 +1,7 @@
 function init() {
   const canvas: HTMLCanvasElement = document.createElement('canvas');
+  canvas.width = 600;
+  canvas.height = 600;
   document.body.appendChild(canvas);
   const webgl = canvas.getContext('webgl');
   webgl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
